34082676cf1cc7a74763d9d2af73c2b106fe3025
Aniket <aniketkaygude2003@gmail.com>
2025-03-27 15:46:12 +0530

    a

--- client/src/hospitalProfile/userProfileSearch.jsx
+++ client/src/hospitalProfile/userProfileSearch.jsx
@@ -307,7 +307,7 @@ function GeneratePrescription({ all_patientData, patient, handleCloseModal }) {
         </div>
 
         <h3 className="text-xl font-semibold mt-6 mb-4">Generate Prescription</h3>
-        <div className="max-h-64 overflow-y-auto border rounded-lg p-4">
+        <div className="max-h-32 overflow-y-auto border rounded-lg p-4">
           {formData.medicines.map((medicine, index) => (
             <div key={index} className="grid grid-cols-5 gap-4 mb-4 items-center">
               <input
@@ -372,9 +372,5 @@ function GeneratePrescription({ all_patientData, patient, handleCloseModal }) {
     </div>
   );
 }
-
-
-
-  
 
 export default UserProfileSearch;
